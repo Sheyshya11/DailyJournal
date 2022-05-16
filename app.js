cfb96ccdc78083c9afd080752873473d4ddbe94a
Sheyshya11 <subhamshrestha60@yahoo.com>
2022-05-16 20:50:50 +0545

    added remove feature and some ui changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var _ = require('lodash');
 
 const ejs = require("ejs");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Welcome to Daily journal where you can record and track your daily activities";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -20,11 +20,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
 
-  
     res.render("home",{homeContent:homeStartingContent,postsContent:posts});
   
-  
- 
 
 })
 
@@ -50,7 +47,24 @@ app.get('/about', (req, res) => {
   res.render("about",{
     aboutContent:aboutContent});
 })
+app.get('/erase/:postid', (req, res) => {
+  const postidName=  _.lowerCase(req.params.postid);
+  
+  posts.forEach(function(title)
+  {
+    const storedTitle=_.lowerCase(title.title);
+    if (postidName===(storedTitle))
+  {   const index=posts.indexOf(title)
+    posts.splice(index,1)
+   
+    res.redirect("/");
+}
 
+})
+
+
+ 
+})
 
 
 app.get('/contact', (req, res) => {
